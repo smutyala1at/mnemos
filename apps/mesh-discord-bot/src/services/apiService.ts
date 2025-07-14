962abda8e71da40ca06935dded82817a3cbf25d1
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-07-14 15:52:00 +0200

    feat(api-service): use axios for http request

--- apps/mesh-discord-bot/src/services/apiService.ts
+++ apps/mesh-discord-bot/src/services/apiService.ts
@@ -1,9 +1,8 @@
+import axios from "axios";
+
 export async function askAI(question: string) {
     const baseUrl = process.env.API_BASE_URL;
     const apiKey = process.env.API_KEY;
-    const body = JSON.stringify({ question: question });
-    console.log("body:", body);
-    console.log("question:", question);
 
     if(!baseUrl || !apiKey) {
         throw new Error("API_BASE_URL and API_KEY must be defined in .env")
@@ -11,19 +10,14 @@ export async function askAI(question: string) {
 
     console.log("making backend call");
 
-    const response = await fetch(`${baseUrl}/api/ask-mesh-ai`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": apiKey!
-        },
-        body
-    });
+    const response = await axios.post(`${baseUrl}/api/ask-mesh-ai`,
+        { question },
+        {
+            headers: {
+                "x-api-key": apiKey
+            }
+        }
+    )
 
-    if(!response.ok) {
-        throw new Error(`API error: ${response.status}`)
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
 }
