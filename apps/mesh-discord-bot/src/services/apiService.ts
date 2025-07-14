95b6e04a575771ac513a551ae169c35db89135e5
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-07-14 15:45:29 +0200

    add logs

--- apps/mesh-discord-bot/src/services/apiService.ts
+++ apps/mesh-discord-bot/src/services/apiService.ts
@@ -2,13 +2,15 @@ export async function askAI(question: string) {
     const baseUrl = process.env.API_BASE_URL;
     const apiKey = process.env.API_KEY;
     const body = JSON.stringify({ question: question });
+    console.log("body:", body);
+    console.log("question:", question);
 
     if(!baseUrl || !apiKey) {
         throw new Error("API_BASE_URL and API_KEY must be defined in .env")
     }
 
     console.log("making backend call");
-    
+
     const response = await fetch(`${baseUrl}/api/ask-mesh-ai`, {
         method: "POST",
         headers: {
